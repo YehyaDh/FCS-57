8cc1155477ec1bcc55b081b12852468c659989ab
Yehya Dhaini <Yehya Dhaini>
2024-10-14 18:16:02 +0300

    Exercise 2

---
+++ Assignments/Assignment 3/Exercise 2.py
@@ -0,0 +1,9 @@
+def generate(list,num1,str1):
+    if num1 == 0:
+        print (str1)
+        return
+    for letter in list:
+        generate(list,num1-1,str1+letter)
+        
+Characters = ["a","b","c"]
+generate(Characters,2,"")
