1b979dab9342a4e70cf675055e08ea01685b818e
Yehya Dhaini <Yehya Dhaini>
2024-10-14 22:06:15 +0300

    update

--- Assignments/Assignment 3/Exercise 2.py
+++ Assignments/Assignment 3/Exercise 2.py
@@ -4,6 +4,8 @@ def generate(list,num1,str1):
         return
     for letter in list:
         generate(list,num1-1,str1+letter)
+ 
+# Test of the function 
         
 Characters = ["a","b","c"]
 generate(Characters,2,"")
